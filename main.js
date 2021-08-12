23e69432b55def1523404f8b3b65ee01a1790b11
Angaar Uriakhil <angar1996@gmail.com>
2021-08-12 15:58:44 +0100

    Fixed item alignment. Responsiveness next

---
+++ main.js
@@ -0,0 +1,26 @@
+AOS.init();
+
+dateInFooter();
+
+function dateInFooter() {
+  let footerInfo = document.getElementById("footer-info");
+  footerInfo.innerHTML = `Developed and maintained by Angaar Uriakhil, ${new Date().getFullYear()}.`;
+};
+
+
+function Placeholder () {
+  let welcome = document.getElementById("span1").innerText;
+  welcome_array = [];
+  for (let i=0; i<welcome.length; i++) {
+    welcome_array.push(welcome[i])
+    };
+  console.log(welcome_array);
+  window.onload = IDontKnowYet;
+  } 
+
+
+
+function scrollBar() {
+  document.getElementById();
+  }
+
