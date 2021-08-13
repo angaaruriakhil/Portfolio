6073af1bf5c9ffce3d0c27b4b5b35805efbd740f
Angaar Uriakhil <angar1996@gmail.com>
2021-08-13 13:35:09 +0100

    playing around with animations in interaction observer

--- main.js
+++ main.js
@@ -18,11 +18,13 @@ function Placeholder () {
   window.onload = IDontKnowYet;
   } 
 
-/* This function uses intersection observer to produce the scroll bar. Read more here. https://usefulangle.com/post/118/javascript-intersection-observer */
+/* This section uses intersection observer to produce the scroll bar. I adapted this solution on stackoverflow for my needs: https://stackoverflow.com/questions/66336096/single-intersection-observer-for-multiple-entries */
+
 let observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       modify(entry.target);
+      pageAnimation(entry.target);
     } else {
       revert(entry.target);
     }
@@ -42,6 +44,20 @@ function revert(el) {
   document.querySelector(`#${el.id}-link`).style.textDecorationColor = 'none';
 }
 
+function pageAnimation(el) {
+  if (el.id == "projects") {
+    document.getElementById("projects__card-1").className += " animate__animated animate__lightSpeedInRight";
+    document.getElementById("projects__card-2").className += " animate__animated animate__lightSpeedInLeft";
+  }
+  if (el.id == "about") {
+    document.getElementById("about__article").className += " animate__animated animate__bounceInRight";
+    document.getElementById("about__tech-stack").className += " animate__animated animate__bounceInLeft";
+  }
+  if (el.id == "contact") {
+    document.getElementById("contact__card__message").className += " animate__animated animate__zoomInDown";
+    document.getElementById("button").className += " animate__animated animate__delay-2s animate__flipInX";
+  }
+}
 
 observer.observe(document.querySelector('#landing'));
 observer.observe(document.querySelector('#about'));
