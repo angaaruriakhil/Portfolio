ad5947eae406e75e56632bf822f887bc92b93249
Angaar Uriakhil <angar1996@gmail.com>
2021-08-13 14:10:16 +0100

    fixed order of animations on about page

--- main.js
+++ main.js
@@ -52,6 +52,11 @@ function pageAnimation(el) {
   if (el.id == "about") {
     document.getElementById("about__article").className += " animate__animated animate__bounceInRight";
     document.getElementById("about__tech-stack").className += " animate__animated animate__bounceInLeft";
+    document.getElementById("hi-icon").className += " animate__animated animate__slower animate__swing animate__delay-1s";
+    document.getElementById("fa-handshake").className += " animate__animated animate__headShake animate__slower animate__delay-3s";
+    document.getElementById("fa-layer-group-1").className += " animate__animated animate__heartBeat animate__delay-4s";
+    document.getElementById("fa-layer-group-2").className += " animate__animated animate__heartBeat animate__delay-4s";
+
   }
   if (el.id == "contact") {
     document.getElementById("contact__card__message").className += " animate__animated animate__zoomInDown";
