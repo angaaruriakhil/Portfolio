71fb83b0755fadc2ee9ec06fb785c414d483b673
Angaar Uriakhil <angar1996@gmail.com>
2021-08-21 16:02:48 +0100

    added JS obfuscation w/ regex and encryption with crypto-js

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,7 +1,6 @@
 "use strict";
 
 AOS.init();
-dateInFooter();
 
 function dateInFooter() {
   var footerInfo = document.getElementById("footer-info");
@@ -9,6 +8,7 @@ function dateInFooter() {
 }
 
 ;
+dateInFooter();
 
 function spinLogoToTop() {
   document.getElementById("fixed_elements__logo-img").className = "fixed_elements__logo-img animate__animated animate__rotateIn";
@@ -114,4 +114,54 @@ function hamburgerMenuAnimation() {
 }
 
 document.getElementById("fixed_elements__hamburger-menu").addEventListener("click", toggleHamburgerMenu);
-document.getElementById("fixed_elements__hamburger-menu").addEventListener("click", hamburgerMenuAnimation);
+document.getElementById("fixed_elements__hamburger-menu").addEventListener("click", hamburgerMenuAnimation); // Email encryption using obfuscation and encryption (Not sure if this will work but worth a shot and its better than just having it openly available)
+
+function locked_link() {
+  var efrac_object = {
+    efrac_0_array: ["m", "!", "a", "%", "i", "l", "t", "*", "o", "+", ":"],
+    efrac_1_array: ["a", "!", "n", "%", "$", "g", "(", "*", "a", "r", "]"],
+    efrac_2_array: ["1", "!", "9", "%", "i", "l", "9", "*", "o", "6", "[]"],
+    efrac_3_array: ["g", "!", "m", "a", "@", "%", "£", "i", "*", "^", "#"],
+    efrac_4_array: ["g", "!", "m", "a", "%", "%", "£", "i", "*", "l", "#"],
+    efrac_5_array: [".", "£", "c", "%", "o", "^", "&", "m", "*", "+", ">"]
+  };
+  var regex_0 = /[a-z:.]/;
+  var regex_1 = /[a-z]/;
+  var regex_2 = /[0-9]/;
+  var regex_3 = /[@]/;
+  filtered_e0 = efrac_object.efrac_0_array.filter(function (i) {
+    return regex_0.test(i);
+  });
+  filtered_e1 = efrac_object.efrac_1_array.filter(function (i) {
+    return regex_1.test(i);
+  });
+  filtered_e2 = efrac_object.efrac_2_array.filter(function (i) {
+    return regex_2.test(i);
+  });
+  filtered_e3 = efrac_object.efrac_3_array.filter(function (i) {
+    return regex_3.test(i);
+  });
+  filtered_e4 = efrac_object.efrac_4_array.filter(function (i) {
+    return regex_1.test(i);
+  });
+  filtered_e5 = efrac_object.efrac_5_array.filter(function (i) {
+    return regex_0.test(i);
+  });
+  var secretKey = "secretKey";
+  var unlocked_array = [filtered_e0.join(""), filtered_e1.join(""), filtered_e2.join(""), filtered_e3.join(""), filtered_e4.join(""), filtered_e5.join("")];
+  var bare_message = unlocked_array[0] + unlocked_array[1] + unlocked_array[2] + unlocked_array[3] + unlocked_array[4] + unlocked_array[5];
+  var encryptedBytes = CryptoJS.AES.encrypt(bare_message, secretKey);
+  return encryptedBytes.toString();
+}
+
+function unlockedLink() {
+  var secretKey = "secretKey";
+  var decryptedBytes = CryptoJS.AES.decrypt(locked_link(), secretKey);
+  var decryptedMessage = decryptedBytes.toString(CryptoJS.enc.Utf8);
+  return decryptedMessage;
+}
+
+function displaceLink() {
+  var contactButton = document.getElementById('button');
+  contactButton.href = unlockedLink();
+}
